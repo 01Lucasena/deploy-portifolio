bfd2c4939f2a7dc299bfd2c80f562ddc67d5eac0
Lucas Sena <159453926+01Lucasena@users.noreply.github.com>
2025-06-16 21:46:06 -0300

    Update Background

--- src/components/page-wrapper/PageWrapper.js
+++ src/components/page-wrapper/PageWrapper.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import background from '../assets/background.png';
+import './PageWrapper.css';
+
 
 export default function PageWrapper({ children }) {
   return (
+    const location = useLocation();
+    const isMapa = location.pathname.includes('mapa');
     <motion.div
+      className={isMapa ? 'wrapper no-bg' : 'wrapper'}
       initial={{ opacity: 0, y: 30 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
